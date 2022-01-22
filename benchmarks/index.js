4ff753e69912e6b8c7e7148e77c3662e04e664d8
Ferdinand Prantl <prantlf@gmail.com>
2022-01-22 14:50:46 +0100

    Benchmnark the latest test framework versions

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -4,7 +4,7 @@ const { promisify } = require('util'),
       runners = {
         ava: ['./node_modules/.bin/ava', 'suites/ava/**'],
         baretest: ['node', 'suites/baretest'],
-        jasmine: ['./node_modules/.bin/jasmine', 'suites/jasmine'],
+        jasmine: ['./node_modules/.bin/jasmine', 'suites/jasmine/index.js'],
         jest: ['./node_modules/.bin/jest', 'suites/jest', '--env=node'],
         mocha: ['./node_modules/.bin/mocha', 'suites/mocha'],
         pta: ['./node_modules/.bin/pta', 'suites/pta/index.js'],
@@ -33,9 +33,9 @@ async function run(name, args) {
 (async () => {
   try {
     for (const name in runners) await run(name, runners[name])
-    console.log('| Feature |', Object.keys(runners).map(name => name.padEnd(9)).join(' | '), '|')
-    console.log('| ------- |' + ' --------- |'.repeat(Object.keys(runners).length))
-    console.log('| Launch  |', Object.values(runners).map(format).join(' | '), '|')
+    console.log('| Feature   |', Object.keys(runners).map(name => name.padEnd(9)).join(' | '), '|')
+    console.log('| --------- |' + ' --------- |'.repeat(Object.keys(runners).length))
+    console.log('| Launch    |', Object.values(runners).map(format).join(' | '), '|')
   } catch (err) {
     console.error(err)
     process.exit(1)
